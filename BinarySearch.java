f4d1ee6fbd3f2a75efe9be2f0f2fdf9393d02ab3
Chandler Lo <chanlosn@gmail.com>
2020-08-09 17:12:35 +0800

    Add

---
+++ BinarySearch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), q = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = in.nextInt();
+
+        return;
+    }
+
+    public static boolean check(int x) {
+        return true;
+    }
+
+    public static int BinarySearch1(int l, int r) {
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (check(mid)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+
+    public static int BinarySearch2(int l, int r) {
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (check(mid)) l = mid;
+            else r = mid - 1;
+        }
+        return r;
+    }
+}
---
+++ Temp.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), q = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = in.nextInt();
+
+        while (q-- != 0) {
+            int x = in.nextInt();
+            System.out.print(BinarySearch1(0, n - 1) + " " + BinarySearch2(0, n - 1));
+        }
+
+        return;
+    }
+
+    public static boolean check(int x) {
+        return true;
+    }
+
+    public static int BinarySearch1(int l, int r) {
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (check(mid)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+
+    public static int BinarySearch2(int l, int r) {
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (check(mid)) l = mid;
+            else r = mid - 1;
+        }
+        return r;
+    }
+}
